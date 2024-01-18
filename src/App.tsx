eb7d9af362597a8f69d9f95cff9b160e4f5acf69
Anna Pastushenko <annpastushenko23@gmail.com>
2024-01-18 21:45:42 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Outlet } from 'react-router-dom';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App: React.FC = () => (
+  <div className="App">
+    <Header />
 
-export default App;
+    <body>
+      <div className="App__main">
+        <Outlet />
+      </div>
+    </body>
+
+    <Footer />
+  </div>
+);
